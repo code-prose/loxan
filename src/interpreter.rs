7f7bf76bc11b509b48dc7407a5468d7abe6cb5ad
Logan Rose <logprose@umich.edu>
2026-04-30 23:35:02 -0400

    feat: basic interpretter impl

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,11 +1,11 @@
 use std::env;
 use std::io;
-use std::io::Read;
 use std::fs;
 
 #[allow(dead_code)]
 pub struct Rlox;
 
+#[allow(dead_code)]
 impl Rlox {
     pub fn main() ->  io::Result<()> {
         let args: Vec<String> = env::args().collect();
@@ -14,15 +14,15 @@ impl Rlox {
               println!("Usage: jlox [script]");
               std::process::exit(64) 
         } else if args.len() == 1 {
-            runFile(args[0]);
+            Rlox::run_file(&args[0]).unwrap()
         } else {
-            runPrompt();
+            Rlox::run_prompt().unwrap()
         }
         
         Ok(())
     }
 
-    fn runFile(path: String) -> io::Result<()> {
+    fn run_file(path: &String) -> io::Result<()> {
         let source = fs::read_to_string(path).unwrap();
         println!("{source:?}");
         Rlox::run(source);
@@ -30,20 +30,19 @@ impl Rlox {
         Ok(())
     }
 
-    fn runPrompt() -> io::Result<()> {
+    fn run_prompt() -> io::Result<()> {
         let mut input = String::new();
-
-        // fix this fuckery
         loop {
             println!("> ");
-            io::stdin().read_line(&mut input);
-            let input = match input {
-                Ok(line) => line,
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {},
                 Err(e) => {
                     println!("Could not interpret line: {e}");
                     break
-                } 
-            }
+                }
+            };
+            if input == "" { break }
+            else { Rlox::run(input.clone()) };
         }
 
         Ok(())
@@ -51,6 +50,11 @@ impl Rlox {
     }
 
     fn run(source: String) {
+        let scanner = Scanner::new(source);
+        let tokens: Vec<Token> = scanner.scanTokens();
 
+        for token in tokens.iter() {
+            println!("{token}")
+        }
     }
 }
