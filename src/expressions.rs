2f931643fba262e18339f580a5797bf31d5cfde9
Logan Rose <logprose@umich.edu>
2026-05-05 23:14:12 -0400

    feat: setup binary expr

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,9 +1,13 @@
+use crate::tokens::Token;
+
 enum Expr {
     Literal {
-
+        
     },
     Binary {
-
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>
     },
     Unary {
 
