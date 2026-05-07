fed26f5b436fa81cae35a4576ed9088a8a64bdd6
Logan Rose <logprose@umich.edu>
2026-05-06 22:18:52 -0400

    feat: pretty print for exprs

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,20 +1,30 @@
-use crate::tokens::{Token, TokenType, Literal};
+use crate::tokens::{Literal, Token, TokenType};
 
 enum Expr {
     Literal {
-       literal: Literal 
+        literal: Literal,
     },
     Binary {
         left: Box<Expr>,
         operator: Token,
-        right: Box<Expr>
+        right: Box<Expr>,
     },
     Unary {
         operator: Token,
-        expr: Box<Expr>
-
+        expr: Box<Expr>,
     },
     Grouping {
-        expr: Box<Expr>
+        expr: Box<Expr>,
     },
+}
+
+pub fn pretty_print(expr: &Expr) -> String {
+    match expr {
+        Expr::Literal { literal } => format!("({literal:?})"),
+        Expr::Grouping { expr } => format!("group {}", pretty_print(expr)),
+        Expr::Unary { operator, expr } => format!("{} {}", operator.lexeme, pretty_print(expr)),
+        Expr::Binary { left, operator, right } => {
+            format!("({} {} {})", operator.lexeme, pretty_print(left), pretty_print(right))
+        },
+    }
 }
