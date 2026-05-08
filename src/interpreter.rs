364025b989c444ec793560ad73d5548e330a6a39
Logan Rose <logprose@umich.edu>
2026-05-07 23:29:41 -0400

    feat: test case for comma grouping

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -118,4 +118,28 @@ mod tests {
         assert_eq!(output_str, "> (- (group (* 3 4)) 1)\n> ")
 
     }
+
+    #[test]
+    fn test_comma_grouping() {
+        let mut rlox = Rlox::new();
+        let input = b"1, 2\n";
+        let mut output = Vec::new();
+
+        let _res = rlox.run_prompt_on(input.as_ref(), &mut output).unwrap();
+
+        let output_str = String::from_utf8(output).unwrap();
+        assert_eq!(output_str, "> (, 1 2)\n> ")
+    }
+
+    #[test]
+    fn test_multiple_comma_grouping() {
+        let mut rlox = Rlox::new();
+        let input = b"1, 2, 3\n";
+        let mut output = Vec::new();
+
+        let _res = rlox.run_prompt_on(input.as_ref(), &mut output).unwrap();
+
+        let output_str = String::from_utf8(output).unwrap();
+        assert_eq!(output_str, "> (, (, 1 2) 3)\n> ")
+    }
 }
