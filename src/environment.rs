17d390b184505c7a765a869300da6672989b5f4a
Logan Rose <logprose@umich.edu>
2026-05-12 22:33:54 -0400

    feat: var impl using refcell for interior mutability

--- src/environment.rs
+++ src/environment.rs
@@ -1,13 +1,21 @@
 // he wants map and hashmap?
 // what is a map?
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use crate::tokens::Literal;
 
-// could I use &'a str?
-// seems sloppy
 pub struct Environment {
-    values: HashMap<String, Literal>
+    values: HashMap<String, RefCell<Literal>>
 }
 
+impl Environment {
+    // I might need to convert it to a RefCell in this function
+    fn define(&mut self, name: String, value: RefCell<Literal>) {
+        self.values.insert(name, value);
+    }
+
+    fn get(&mut self, name: String) -> &RefCell<Literal> {
+        self.values.get(&name).expect(format!("Variable '{name}' is undefined.").as_str())
+    }
+}
 
